73d16c11cc4268a269bbc5536a963db28529adb3
Ivan Korotkov <twee@tweedle-dee.org>
2009-10-11 20:05:16 +0400

    Added lost @#irc{}@ definition (see r14).

--- include/irc.hrl
+++ include/irc.hrl
@@ -1,3 +1,9 @@
 -define(USER(Nick), {Nick, _, _}).
 -define(TOPIC(Topic), {Topic, _, _}).
 -define(IS_CHAN(ChanName), hd(ChanName) =:= $# orelse hd(ChanName) =:= $& orelse hd(ChanName) =:= $+ orelse hd(ChanName) =:= $!).
+
+%% This record is passed to all IRC behaviours
+-record(irc, {nick      :: list(),
+			  login     :: list(),
+			  is_oper   :: boolean(),
+			  conn_ref  :: pid()}).
