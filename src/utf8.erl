f7d2a1ac699f36a41dfe4b496ee41f4810ec9a7d
Ivan Korotkov <twee@tweedle-dee.org>
2009-10-15 18:27:44 +0400

    * Removed Unicode characters support (@'CHAR':Char@) from @utf8@. * Added edoc to @utf8@ module.

--- src/utf8.erl
+++ src/utf8.erl
@@ -1,12 +1,57 @@
+%%%-------------------------------------------------------------------
+%%% File    : utf8.erl
+%%% Author  : Ivan Korotkov <twee@tweedle-dee.org>
+%%% Created : 15 Oct 2009 by Ivan Korotkov <twee@tweedle-dee.org>
+%%%-------------------------------------------------------------------
+
+%%% @doc This module provides basic UTF8-handling routines and source "UTF-ication".
+%%% For the latter, add `-compile([{parse_transform, utf8}])' to your source.
+%%% All Latin1 string constants will be automatically replaced with Unicode strings.
+%%% To keep some string constant as Latin1-encoded, use form 'LATIN':"latin1-string".
+
 -module(utf8).
+-author("Ivan Korotkov <twee@tweedle-dee.org>").
 
--export([encode/1, decode/1, try_decode/1, split/2, parse_transform/2, format_error/1]).
+%% External API
+-export([encode/1, decode/1, try_decode/1, split/2]).
 
+%% parse_transform entry point
+-export([parse_transform/2]).
+
+%% @type charlist() = [unicode_char() | unicode_binary() | charlist()].
+%%   A unicode_binary is allowed as the tail of the list
+%% @type unicode_binary() = binary().
+%%   Characters in binary are encoded in UTF-8 coding standard.
+%% @type unicode_char() = integer().
+%%   Represents valid unicode codepoint.
+
+%% @doc Converts Unicode string to UTF8 binary
+%% @spec encode(Text) -> Binary
+%% where
+%%    Text = charlist()
+%%    Binary = unicode_binary()
 encode(Text) ->
 	unicode:characters_to_binary(Text).
 
+%% @doc Converts UTF8 binary to Unicode string, skipping invalid bytes
+%% @spec decode(Binary) -> Text
+%% where
+%%    Binary = unicode_binary()
+%%    Text = charlist()
 decode(Binary) ->
 	case binary_to_characters(Binary, skip) of {utf8, Text} -> Text end.
+
+%% @doc Tries to convert Latin1 string to Unicode string.
+%%      Returns original string if conversion not possible.
+%% @spec try_decode(Text) -> UtfText
+%% where
+%%    Text = string()
+%%    UtfText = charlist() | string()
+try_decode(Text) when is_list(Text) ->
+	case binary_to_characters(list_to_binary(Text), abort) of
+		{utf8, UtfText} -> UtfText;
+		not_utf8 -> Text
+	end.
 
 binary_to_characters(Bin, OnError) ->
 	binary_to_characters(Bin, OnError, []).
@@ -20,20 +65,16 @@ binary_to_characters(<<_Invalid:8, _Rest/binary>>, abort, _) ->
 binary_to_characters(<<>>, _, List) ->
 	{utf8, lists:reverse(List)}.
 
-try_decode(Text) when is_list(Text) ->
-	case binary_to_characters(list_to_binary(Text), abort) of
-		{utf8, UtfText} -> UtfText;
-		not_utf8 -> Text
-	end.
-
+%% @doc Splits UTF8 binary into two valid UTF8 parts of specified size
+%%   First part will have closest possible size to Len but not exceeding it.
+%% @spec split(Binary, Len) -> {Head, Rest}
+%% where
+%%    Binary = unicode_binary()
+%%    Head = unicode_binary()
+%%    Rest = unicode_binary()
+%%    Len = integer()
 split(Binary, Len) when is_binary(Binary), Len > 1 ->
 	split_once(Binary, Len).
-
-%% split(<<>>, _, Result) ->
-%% 	lists:reverse(Result);
-%% split(Binary, Len, Result) ->
-%% 	{Head, Rest} = split_once(Binary, Len),
-%% 	split(Rest, Len, [Head | Result]).
 
 split_once(Binary, Len) when size(Binary) =< Len ->
 	{Binary, <<>>};
@@ -50,17 +91,13 @@ check_split(Head, Rest) ->
 			check_split(NewHead, <<Tail/binary, Rest/binary>>)
 	end.
 
+%% @private
+%% @doc Transforms AST into new one, replacing Latin string constants with Unicode strings
+%% @spec parse_transform(list(), list()) -> list()
 parse_transform(Tree, _Options) ->
-	io:format("Tree: ~p, options: ~p~n", [Tree, _Options]),
-	Tree2 = induce_errors(parse_node(Tree)),
-	io:format("New tree: ~p~n", [Tree2]),
-	Tree2.
+	%io:format("Tree: ~p, options: ~p~n", [Tree, _Options]),
+	parse_node(Tree).
 
-parse_node({remote, _, {atom, _, 'CHAR'}, {atom, Line, Char}}) ->
-	case try_decode(atom_to_list(Char)) of
-		[CP] -> {char, Line, CP};
-		_Err -> put_error(Line, ["not a Unicode character: ~ts", [Char]])
-	end;
 parse_node({remote, _, {atom, _, 'LATIN'}, {string, Line, LatinString}}) ->
 	{string, Line, LatinString};
 parse_node({string, Line, String}) ->
@@ -71,23 +108,3 @@ parse_node(Node) when is_tuple(Node) ->
 	list_to_tuple([parse_node(Elmt) || Elmt <- tuple_to_list(Node)]);
 parse_node(Node) ->
 	Node.
-
--define(ERR_KEY, utf8_transform_errors).
-
-put_error(Line, What) ->
-	Error = {error, {Line, ?MODULE, What}},
-	Errors = case get(?ERR_KEY) of
-				 undefined -> [Error];
-				 List      -> [Error | List]
-			 end,
-	put(?ERR_KEY, Errors),
-	{atom, Line, error_here}.
-
-induce_errors(Tree) ->
-	case get(?ERR_KEY) of
-		undefined -> Tree;
-		Errors    -> erase(?ERR_KEY), Errors ++ Tree
-	end.
-
-format_error([Fmt, Args]) ->
-	io_lib:format(Fmt, Args).
