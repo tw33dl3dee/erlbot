7960332fa681e714b0fa50d61deb1aff75116e89
Ivan Korotkov <twee@tweedle-dee.org>
2010-04-04 21:51:11 +0400

    UUID generation on demand.

--- src/bhv_misc.erl
+++ src/bhv_misc.erl
@@ -23,6 +23,7 @@ help(chancmd) ->
 	 {"ping",			"пинг бота (хз нах надо)"},
 	 {"dice <число>",	"бросок кубика (от 1 до N)"},
 	 {"kickme",			"оригинальный способ уйти с канала"},
+	 {"uuid",           "сгенерировать UUID (уникальный, под ответственность бота)"},
 	 {"id (identify)",	"дать боту возможность рассказать, кто он такой"}];
 help(privcmd) ->
 	none;
@@ -36,6 +37,8 @@ handle_event(cmdevent, {chancmd, Chan, _, ["jabberwock"]}, Irc) ->
 	jabberwock(Irc, Chan);
 handle_event(cmdevent, {chancmd, Chan, _, ["uptime"]}, Irc) ->
 	show_uptime(Irc, Chan);
+handle_event(cmdevent, {chancmd, Chan, ?USER(Nick), ["uuid"]}, Irc) ->
+	gen_uuid(Irc, Chan, Nick);
 handle_event(cmdevent, {chancmd, Chan, _, ["time"]}, Irc) ->
 	{success, [Time]} = util:system("date '+%a %b %d %R:%S %Z %Y'"),
 	ok = irc_conn:chanmsg(Irc, Chan, ["Точное время: ", Time, "."]);
@@ -81,6 +84,10 @@ jabberwock(Irc, Chan) ->
 	irc_conn:chanmsg(Irc, Chan, "Кхм кхм."),
 	timer:sleep(?JABBERWOCK_DELAY),
 	ok = irc_conn:chanmsg(Irc, Chan, "А вот ХУЙ вам, мне лениво.").
+
+gen_uuid(Irc, Chan, Nick) ->
+	{success, [Uuid]} = util:system("uuidgen"),
+	ok = irc_conn:chanmsg(Irc, Chan, [Nick, ": ", Uuid]).
 
 %%	{ok, Data} = file:read_file(?JABBERWOCK_FILE),
 %%	{ok, Lines} = regexp:split(binary_to_list(Data), "\n"),
