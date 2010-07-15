c3f6e00bbefb71bc7c4bd517bdf70414af28a537
Ivan Korotkov <twee@tweedle-dee.org>
2010-07-15 23:16:35 +0400

    Random generic blurps replaced with markov-generated sentences (for #75).

--- src/bhv_blurp.erl
+++ src/bhv_blurp.erl
@@ -20,20 +20,19 @@ help(_) -> none.
 
 %% Bot can react to any `genmsg', direct or induced from `maybe_appeal'.
 handle_event(_, {genmsg, Chan, _User, Msg}, Irc) ->
-	blurp(Irc, Chan, Msg);
+	do_blurp(Irc, Chan, Msg);
 handle_event(_, {Event, Chan, _, _, _}, Irc) when Event =:= mode; Event =:= mymode ->
-	blurp(Irc, Chan, none);
+	do_blurp(Irc, Chan, none);
 handle_event(_, {nick, Chan, _, _}, Irc) ->
-	blurp(Irc, Chan, none);
+	do_blurp(Irc, Chan, none);
 handle_event(_Type, _Event, _Irc) ->
 	not_handled.
 
 -define(BLURP_DELAY, 1000).   % msec
 
--define(BLURP_GENERIC, [40,   % 1/40th probability of comment
-						"Хамите", "Хо-хо!", "Знаменито", "Мрак", "Жуть", "Не учите меня жить", 
-						"Как ребёнка", "Кр-р-расота!", "Толстый и красивый", "Поедем на извозчике",
-						"Поедем на таксо", "У вас вся спина белая", "Подумаешь!", "Ого!"]).
+-define(BLURP_GENERIC, [30,   % 1/30th probability of comment
+						% use Markov generator
+						markov]).  
 
 -define(BLURP_CTX, [["квак|q3", 10,      % 1/10
 					 "Пры-ы-ы-ыжка!", "Заперчатили... =(", "Кваку фтопку, Диабла лучше!"],
@@ -44,21 +43,18 @@ handle_event(_Type, _Event, _Irc) ->
 					["спокойн.*ночи", 5, % 1/5
 					 "Спокойной ночи, приятных снов, желаю увидеть ослов и козлов!"]]).
 
-blurp(Irc, Chan, Message) ->
+do_blurp(Irc, Chan, Message) ->
 	[RevProb | Words] = match_ctx(Message),
 	case choice:make([{1, do}, {RevProb - 1, dont}]) of
 		do ->
 			timer:sleep(?BLURP_DELAY),
-			irc_conn:chanmsg(Irc, Chan, hist, choice:make(Words)),
-			ok;
+			print_blurp(Irc, Chan, Message, Words);
 		dont ->
 			not_handled
 	end.
 
-match_ctx(none) ->
-	?BLURP_GENERIC;
-match_ctx(Message) ->
-	match_ctx(Message, ?BLURP_CTX).
+match_ctx(none)    -> ?BLURP_GENERIC;
+match_ctx(Message) -> match_ctx(Message, ?BLURP_CTX).
 
 match_ctx(_, []) ->
 	match_ctx(none);
@@ -69,3 +65,8 @@ match_ctx(Msg, [[Pattern | Matches] | Rest]) ->
 		false ->
 			match_ctx(Msg, Rest)
 	end.
+
+print_blurp(_Irc, Chan, Message, [markov]) ->
+	{new_event, customevent, {markov, Chan, Message}, undefined};
+print_blurp(Irc, Chan, _Message, Words) ->
+	ok = irc_conn:chanmsg(Irc, Chan, hist, choice:make(Words)).
