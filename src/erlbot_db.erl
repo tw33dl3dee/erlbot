12d9053beeeef67f5644eaf5407f52e99af3b197
Ivan Korotkov <twee@tweedle-dee.org>
2010-08-02 14:02:14 +0400

    Added design documents auto-deployment.

--- src/erlbot_db.erl
+++ src/erlbot_db.erl
@@ -12,7 +12,7 @@
 %% API
 -export([start_link/0]).
 -export([create_sequence/0, create_sequence/1, init_sequence/2, sequence/1, sequence/2, init_db/0, init_table/2]).
--export([query_view/2]).
+-export([query_view/2, create_design_docs/0]).
 
 -include("couchbeam.hrl").
 
@@ -26,13 +26,17 @@ start_link() ->
 	Params = erlbot_config:get_value(couchdb_params, required),
 	DbName = erlbot_config:get_value(couchdb_dbname, required),
 	DbParam = #couchdb_params{name = erlbot,
-							  host = proplists:get_value(host, Params),
-							  username = proplists:get_value(username, Params),
-							  password = proplists:get_value(password, Params)},
+							  host = proplists:get_value(host, Params, "127.0.0.1"),
+							  username = proplists:get_value(username, Params, nil),
+							  password = proplists:get_value(password, Params, nil)},
 	case couchbeam_server:start_connection_link(DbParam) of
 		Pid when is_pid(Pid) -> 
-			couchbeam_server:open_db(Pid, {?MODULE, DbName}),
-			{ok, Pid};
+			case couchbeam_server:open_or_create_db(Pid, {?MODULE, DbName}) of
+				DbPid when is_pid(DbPid) ->
+					ok = create_design_docs(),
+					{ok, Pid};
+				Err1 -> Err1
+			end;
 		Res -> Res
 	end.
 
@@ -46,6 +50,20 @@ query_view(ViewName, Params) ->
 						{error, Err};
 		Result -> Result
 	end.
+
+-define(DESIGN_DOC_DIR, "priv/couchdb/design").
+
+%% Create all design docs listed in `DESIGN_DOC_DIR'
+create_design_docs() ->
+	filelib:fold_files(?DESIGN_DOC_DIR, "\\.json$", %"
+					   false,
+					   fun (File, Acc) ->
+							   case save_design_doc(File) of
+								   ok  -> Acc;
+								   Err -> Err
+							   end
+					   end, 
+					   ok).
 
 init_db() ->
 	Node = node(),
@@ -78,3 +96,11 @@ sequence(Table, Inc) -> mnesia:dirty_update_counter(sequence, Table, Inc).
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+save_design_doc(FilePath) ->
+	{ok, Json} = file:read_file(FilePath),
+	case couchbeam_db:save_doc(?MODULE, couchbeam:json_decode(Json)) of
+		{_}      -> ok;
+		conflict -> ok;  %% doc already saved
+		Err      -> io:format("~p~n", [Err])
+	end.
