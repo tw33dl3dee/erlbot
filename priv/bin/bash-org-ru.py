cf608ea3a206c9c35d26a29ed5c605e39977d2dc
Ivan Korotkov <twee@tweedle-dee.org>
2012-10-16 16:37:14 +0400

    Update @bash.org.ru@ handler to @bash.im@

--- priv/bin/bash-org-ru.py
+++ priv/bin/bash-org-ru.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python2.7
 
 import urllib, urllib2
 from pyquery import PyQuery
@@ -11,23 +11,33 @@ class BashOrgRu(object):
     """Citations, search over bash.org.ru
     """
     _encoding = 'windows-1251'
+    _ua = 'Mozilla/5.0'
     
     def _fetch_page(self, url):
-        return PyQuery(url=url, opener=lambda url: unicode(urllib2.urlopen(url).read(), self._encoding))
+        headers = {'User-Agent': self._ua}
+        return PyQuery(url=url, opener=lambda url: unicode(
+                urllib2.urlopen(urllib2.Request(url, None, headers)).read(),
+                self._encoding))
 
     def _quote_to_text(self, quote_el):
         """Converts quote element to plain text
         """
-        fixed_html = quote_el.html().replace('<br/>', '\n').replace('<', '&lt;').replace('>', '&gt;')
-        return PyQuery(fixed_html).text()        
+        fixed_html = (quote_el.html()
+                      .replace('<br/>', '\n')
+                      .replace('<', '&lt;')
+                      .replace('>', '&gt;'))
+        return PyQuery(fixed_html).text()
 
     def cite(self, num):
         """Get quote by number
+
+        :param num: quote number
+        :return: str
         """
-        page = self._fetch_page("http://bash.org.ru/quote/%d" % num)
+        page = self._fetch_page("http://bash.im/quote/%d" % num)
         if page("#bar"):
             return None
-        quote = page("#quotes div").eq(2)
+        quote = page(".quote .text").eq(0)
         return self._quote_to_text(quote)
 
     def search(self, text, results = 3):
@@ -35,24 +45,24 @@ class BashOrgRu(object):
 
         :param text: text to search (ascii or unicode)
         :param result: number of results to return
-        :return: 
+        :return: list((number, votes, text))
         """
-        url = "http://bash.org.ru/?%s" % urllib.urlencode({'text': text.encode('windows-1251')})
+        url = "http://bash.im/?%s" % urllib.urlencode(
+            {'text': text.encode('windows-1251')})
         page = self._fetch_page(url)
-        # If there is a span with "error" class having "!" in itself, nothing's found
-        # There might also be no span.error at all -- BOR's bug :(
-        if not len(page("span.error")) or True in page("span.error").map(lambda i, e: "!" in PyQuery(e).text()):
+        if not page(".search-results"):
             return []
         # Whoa...
-        matches = [(e.parent()("span").text(),          # votes
-                    int(e.parent()("a").eq(0).text()),  # quote number
-                    self._quote_to_text(e))             # quote text
-                   for m in page("#quotes div.q div.vote + div")
-                   for e in (PyQuery(m),)]
+        matches = [
+            (int(e('.actions .id').eq(0).text().lstrip('#')),  # quote number
+             int(e('.actions .rating').eq(0).text()),          # votes
+             self._quote_to_text(e('.text').eq(0)))            # quote text
+            for m in page('.quote') for e in (PyQuery(m),) if e('.text')]
         return sorted(matches, reverse=True)[0:results]
 
 def main():
-    parser = OptionParser(usage="usage: %prog [options] [TEXT]", version="%prog 0.1")
+    parser = OptionParser(usage="usage: %prog [options] [TEXT]",
+                          version="%prog 0.2")
     parser.add_option("-s", "--search", dest="search", action="store_true",
                       help="search for TEXT in quotes", default=False)
     parser.add_option("-n", "--number", dest="num", type="int", metavar="#",
@@ -62,8 +72,8 @@ def main():
     bor = BashOrgRu()
     if opts.search:
         results = bor.search(text)
-        for votes, num, quote in results:
-            line = "#%d [%s]\n%s\n" % (num, votes, quote)
+        for num, votes, quote in results:
+            line = "#%d [%d]\n%s\n" % (num, votes, quote)
             print line.encode('utf8')
     elif opts.num:
         cite = bor.cite(opts.num)
