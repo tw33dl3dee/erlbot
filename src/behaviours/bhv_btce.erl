d834383b2d3abf02af81575ad7baf47e787ba1aa
Ivan Korotkov <twee@tweedle-dee.org>
2013-11-30 15:12:00 +0400

    rate watcher bugz, multiple rates

--- src/behaviours/bhv_btce.erl
+++ src/behaviours/bhv_btce.erl
@@ -8,14 +8,14 @@
 -module(bhv_btce).
 
 -behaviour(erlbot_behaviour).
--export([init/1, help/1, handle_event/4, config_change/2, diff_rates/3]).
+-export([init/1, help/1, handle_event/4, config_change/2]).
 
 -include("utf8.hrl").
 -include("irc.hrl").
 
 %% Period between subsequent rate checks (msec).
 -define(INITIAL_CHECK_PERIOD, 5000).
--define(CHECK_PERIOD, 300000).
+-define(CHECK_PERIOD, 10000).
 
 init({Chans, Rates}) -> {false, [], Chans, Rates}.
 
@@ -26,17 +26,16 @@ config_change({Chans, Rates}, {InitDone, LastRates, _, _}) ->
 help(_) -> none.
 
 handle_event(_, _, _, {false, _, Chans, Rates}) ->
-    {delayed_event, ?INITIAL_CHECK_PERIOD, customevent, check_rates,
-     {true, [], Chans, Rates}};
+    {delayed_event, ?INITIAL_CHECK_PERIOD, customevent, check_rates, {true, [], Chans, Rates}};
 handle_event(customevent, check_rates, _, {_, LastRates, Chans, Rates} = D) ->
     NewRates = get_rates(Rates),
     case diff_rates(LastRates, NewRates, Rates) of
-        [] -> {delayed_event, ?CHECK_PERIOD, customevent, check_rates, D};
+        [] ->
+            {delayed_event, ?CHECK_PERIOD, customevent, check_rates, D};
         ChangedRates ->
-            [announce_change(Chan, Change)
-             || Chan <- Chans, Change <- ChangedRates],
+            [announce_change(Chan, Change) || Chan <- Chans, Change <- ChangedRates],
             {delayed_event, ?CHECK_PERIOD, customevent, check_rates,
-             {true, NewRates, Chans, Rates}}
+             {true, update_rates(LastRates, ChangedRates, Rates), Chans, Rates}}
     end;
 handle_event(cmdevent, {chancmd, Chan, _, ["btce", Rate]}, _, _) ->
     ok = bhv_common:pipe_script(Chan, "btc-e.py", cmd_args(Rate));
@@ -45,9 +44,12 @@ handle_event(_Type, _Event, _IrcState, _Data) ->
 
 get_rates([{Rate, RateCode, _} | T]) -> 
 	% BUG: use code:priv_dir
-    {success, [Price]} =
-        erlbot_util:execv("btc-e.py", cmd_args(RateCode), "priv/bin"),
-    get_rates(T) ++ [{Rate, list_to_number(Price)}];
+    case erlbot_util:execv("btc-e.py", cmd_args(RateCode), "priv/bin") of
+        {success, [Price]} when length(Price) > 0 ->
+            [{Rate, list_to_number(Price)} | get_rates(T)];
+        _ ->
+            get_rates(T)
+    end;
 get_rates([]) -> [].
 
 cmd_args(Rate) -> ["-r", Rate, "-f", "buy"].
@@ -59,10 +61,9 @@ list_to_number(Str) ->
 
 -define(RATE(Rate), {Rate, _, _}).
 
-diff_rates([{Rate, Price1} | T1], [{Rate, Price2} | T2],
-           [{Rate, _, MaxDiff} | T3]) when abs(Price1 - Price2) > MaxDiff ->
-    ok = error_logger:info_report([irc_event, {change, [Rate, abs(Price1 - Price2), MaxDiff]}]),
-    diff_rates(T1, T2, T3) ++ [{Rate, Price1, Price2}];
+diff_rates([{Rate, Price1} | T1], [{Rate, Price2} | T2], [{Rate, _, MaxDiff} | T3])
+  when abs(Price1 - Price2) > MaxDiff ->
+    [{Rate, Price1, Price2} | diff_rates(T1, T2, T3)];
 %% rate unchanged
 diff_rates([{Rate, _} | T1], [{Rate, _} | T2], [?RATE(Rate) | T3]) ->
     diff_rates(T1, T2, T3);
@@ -71,25 +72,29 @@ diff_rates([{Rate, _} | T1], New, [?RATE(Rate) | T3]) ->
     diff_rates(T1, New, T3);
 %% new rate (announce initially)
 diff_rates(Old, [{Rate, Price} | T2], [?RATE(Rate) | T3]) ->
-    diff_rates(Old, T2, T3) ++ [{Rate, Price}];
+    [{Rate, Price} | diff_rates(Old, T2, T3)];
 %% no longer watching this rate, skip it
 diff_rates([_ | T1], New, Rates) ->
     diff_rates(T1, New, Rates);
 diff_rates([], [], []) -> [].
 
+update_rates(LastRates, [{Rate, InitialPrice} | T2], [{Rate, _, _} | T3]) ->
+    [{Rate, InitialPrice} | update_rates(LastRates, T2, T3)];
+update_rates([{Rate, _} | T1], [{Rate, _, NewPrice} | T2], [{Rate, _, _} | T3]) ->
+    [{Rate, NewPrice} | update_rates(T1, T2, T3)];
+update_rates([{Rate, _} = Unchanged | T1], ChangedRates, [{Rate, _, _} | T3]) ->
+    [Unchanged | update_rates(T1, ChangedRates, T3)];
+update_rates([_ | T1], ChangedRates, Rates) ->
+    update_rates(T1, ChangedRates, Rates);
+update_rates([], [], []) -> [].
+
 announce_change(Chan, {Rate, InitialPrice}) ->
-    ok = irc_conn:chanmsg(Chan, hist,
-                          % bold
-                          [2, Rate, ": ", price_to_list(InitialPrice), 15]);
+    ok = irc_conn:chanmsg(Chan, hist, [2, Rate, ": ", price_to_list(InitialPrice), 15]);  % bold
 announce_change(Chan, {Rate, Price1, Price2}) when Price1 > Price2 ->
-    ok = irc_conn:chanmsg(Chan, hist,
-                          % bold, green
-                          [2, Rate, ": ", price_to_list(Price1),
-                           " -> ", 3, "03", price_to_list(Price2), 15]);
+    ok = irc_conn:chanmsg(Chan, hist, [2, Rate, ": ", price_to_list(Price1), " -> ",
+                                       3, "03", price_to_list(Price2), 15]);  % bold, green
 announce_change(Chan, {Rate, Price1, Price2}) ->
-    ok = irc_conn:chanmsg(Chan, hist,
-                          % bold, red
-                          [2, Rate, ": ", price_to_list(Price1),
-                           " -> ", 3, "04", price_to_list(Price2), 15]).
+    ok = irc_conn:chanmsg(Chan, hist, [2, Rate, ": ", price_to_list(Price1),
+                                       " -> ", 3, "04", price_to_list(Price2), 15]).  % bold, red
 
 price_to_list(Price) -> io_lib:format("~.2f", [Price]).
